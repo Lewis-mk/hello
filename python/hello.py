7571f5a2e6332c5a3342975c7f208eb18477fe3a
lewismwaura <lewismwaura2021@gmail.com>
2023-12-30 12:22:37 +0300

    list in python

---
+++ python/hello.py
@@ -0,0 +1,29 @@
+# name = input("Name: ")
+# age = int(input("Age: "))
+#
+#
+# if age > 18:
+#     print(f"{name} your age is {age}, you are eligible for voting.")
+# else:
+#     print(f"{name} your age is {age}, you are not eligible for voting.")
+#
+# for _ in range(10):
+#     if _ % 2 == 0:
+#         print(f"{_} is even.")
+#     else:
+#         print(f"{_} is odd.")
+
+# define a list of names
+# names = ['lewis', 'lenny', 'david', 'sarah']
+#
+# for name in names:
+#     print(f'{name}')
+#
+# # create a empty set
+# s = set()
+#
+# for _ in range(10):
+#     s.add(_)
+#     if _ == 2:
+#         s.remove(2)
+# print(f"there are {len(s)} elements which are",s)
